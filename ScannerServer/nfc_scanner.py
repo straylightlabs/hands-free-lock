8efa23b792726c90942dd7967ec3211708b01595
Ryo Kawaguchi <ryo@straylight.jp>
2017-01-24 05:28:31 +0900

    Add ScannerServer.

---
+++ ScannerServer/nfc_scanner.py
@@ -0,0 +1,44 @@
+import signal
+import time
+
+from pirc522 import RFID
+
+loop = True
+rfid = RFID()
+
+def end_read(signal, frame):
+    global loop
+    print("\nCtrl+C captured, ending read.")
+    loop = False
+    rfid.cleanup()
+
+signal.signal(signal.SIGINT, end_read)
+
+print("Scanning...")
+
+while loop:
+    (error, data) = rfid.request()
+    if error:
+        print(error)
+        continue
+
+    (error, uid) = rfid.anticoll()
+    if error:
+        print(error)
+        continue
+
+    rfid.select_tag(uid)
+    data = rfid.read(4)[-4:]
+    done_reading = False
+    for i in xrange(2, 16):
+        for c in rfid.read(i * 4):
+            if c == 0:
+                done_reading = True
+                break
+            data.append(c)
+        if done_reading:
+            break
+        
+    print(''.join([chr(c) for c in data]))
+    time.sleep(1)
+
